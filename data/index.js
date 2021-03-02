f3ea96c4aabfd6256e3ed27669f0ba2971246f93
ahmedallani <ahmed.allani@rbk.tn>
2021-03-02 15:18:50 -0800

    pairs

---
+++ data/index.js
@@ -0,0 +1,19 @@
+export default [
+  ["Marwen Trabelsi", "Mohamed Seif Bardi"],
+  ["Amir Hachani", "Nedia Khelifi"],
+  ["Youssef Talhewi", "Hiba Bouslimi"],
+  ["Mohamed Aziz Jelazi", "Baha Nouali"],
+  ["Mohamed Rezgui", "Ahmed Gharbi"],
+  ["Oussema Yahyaoui", "Karim Zargouni"],
+  ["Youssef Mhamdi", "Aziz Tounsi"],
+  ["Mohamed Islam Boudhina", "Wael Hajji"],
+  ["Mohamed Chebi", "Rafik Boubaker"],
+  ["Mehdi Hassoun", "Sabrine Noemane"],
+  ["Abdelhamid Miled", "Sabrine Douihech"],
+  ["Mohamed Ashref Gallah", "Mayssa Ben Yaala"],
+  ["Hedi Riba", "Oussema Chemingui"],
+  ["Aziz Khardani", "Amal Ben Sassi"],
+  ["Arij Laatigue", "Hamza Fendri"],
+  ["Lokmen Abid", "Mohamed Karam Saadaoui"],
+  ["Abdenacer Zin Elabidin", "Malek Hbib Hizaoui"]
+];
--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,47 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        fullpairs
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="bg-gray-100 min-h-screen grid grid-cols-3 gap-2 p-2 text-center">
+    <div
+      v-for="(table, index) in tables"
+      :key="index"
+      class="bg-white rounded-md flex flex flex-col items-center justify-around p-2"
+    >
+      <div
+        v-for="(pair, i) in table"
+        :key="i"
+        class="w-11/12 flex rounded-md flex-wrap justify-around p-2 m-2  bg-gray-100"
+      >
+        <div>{{ pair[0] }}</div>
+        <img :src="icons.handshake" />
+        <div>{{ pair[1] }}</div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import pairs from "@/data";
+export default {
+  data() {
+    return {
+      pairs,
+      icons: {
+        handshake:
+          "https://a.slack-edge.com/production-standard-emoji-assets/13.0/google-medium/1f91d.png"
+      }
+    };
+  },
+  computed: {
+    tables: function() {
+      let arr = [];
+      let i = 0;
+      while (i < this.pairs.length - 2) {
+        arr.push([this.pairs[i], this.pairs[i + 1]]);
+        i += 2;
+      }
+      return arr;
+    }
+  }
+};
 </script>
 
 <style>
@@ -47,16 +60,8 @@ export default {}
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
